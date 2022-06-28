e886bb4d30c929e065ea4e39c98f30504be0e592
Pablo <pabloh.anselmo@gmail.com>
2022-06-28 18:48:49 -0300

    script para adicionar usuario para swagger

--- src/main/resources/db/migration/postgresql/V2__swagger-user.sql
+++ src/main/resources/db/migration/postgresql/V2__swagger-user.sql
@@ -1,2 +1,2 @@
-insert into users(name, email)
-values('swagger user', 'development@swagger.user');
+insert into users(NAME, PASSWORD, EMAIL)
+values('swagger user', '123456789', 'development@swagger.user');
